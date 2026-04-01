f62b5ed93c23d6d3cfb7f6cb1b896c0eb447b531
giannashamone-ctrl <giannashamone@gmail.com>
2026-04-01 12:57:35 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>My App is Live 🚀</h1>
+      <p>This is my homepage.</p>
+    </main>
+  );
+}
